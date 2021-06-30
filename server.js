9a1e294c72d81fd52d0e08ca87a8b80195d5f5e2
richard-d-lee <richdelee@gmail.com>
2021-06-30 10:38:37 -0600

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const PORT = process.env.PORT || 3020;
-const User = mongoose.model('user', { name: String });
+const Cat = mongoose.model('Cat', { name: String });
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/test',
@@ -14,14 +14,14 @@ mongoose.connect(
   },
 );
 
-app.post('/test', (req, res) => {
-  const kitty = new User({ name: 'Zildjian' });
-  kitty.save().then((err) => console.log(err));
+app.get('/test', (req, res) => {
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
   res.send("Saved!!!!")
 })
 
-app.use(express.static('./client/dist')); // Host your dist folder up to the server
-app.use(express.json()); // Alternative to BodyParser
+app.use(express.static('./client/dist'));
+app.use(express.json());
 
 // Listening for requests on the PORT
 app.listen(PORT, () => {
