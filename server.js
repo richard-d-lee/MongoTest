008022265bc1e8d0369170e4b7e744202beca352
richard-d-lee <richdelee@gmail.com>
2021-06-30 10:30:11 -0600

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const PORT = process.env.PORT || 3020;
-const Cat = mongoose.model('Cat', { name: String });
+const User = mongoose.model('user', { name: String });
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/test',
@@ -15,8 +15,8 @@ mongoose.connect(
 );
 
 app.post('/test', (req, res) => {
-  const kitty = new Cat({ name: 'Zildjian' });
-  kitty.save().then(() => console.log('meow'));
+  const kitty = new User({ name: 'Zildjian' });
+  kitty.save().then((err) => console.log(err));
   res.send("Saved!!!!")
 })
 
