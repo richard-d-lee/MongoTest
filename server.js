d0b72a353b1957050ddc92ef88e2e3989c735a48
richard-d-lee <richdelee@gmail.com>
2021-06-30 08:16:31 -0600

    Pushing to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3020;
+const PORT = process.env.PORT || 3020;
 
 app.use(express.static('./client/dist')); // Host your dist folder up to the server
 app.use(express.json()); // Alternative to BodyParser
